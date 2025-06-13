21a78ec78222941602f673d258f05befb760a4db
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-06-13 19:45:04 -0300

    criando a funçao de limpar os campos

---
+++ main.js
@@ -0,0 +1,12 @@
+// Limpa todos os campos do formulário de cadastro
+const limpar = () => {
+    document.getElementById("nome").value = "";
+    document.getElementById("titulo").value = "";
+    document.getElementById("descricao").value = "";
+    document.getElementById("contato").value = "";
+    document.getElementById("cep").value = "";
+    document.getElementById("rua").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("estado").value = "";
+}
